75dd428b3bb00a272a8072eb1fdc81fe139f75f1
ZackFiner <zackary.finer@yahoo.com>
2019-09-22 02:21:57 -0700

    Update quadTree.h

--- quadTree.h
+++ quadTree.h
@@ -1,10 +1,26 @@
 #pragma once
+#include "ofMain.h"
+#include "EID.h"
+class collisionHull;
 class quadRegion
 {
-
+	/*
+	I've opted to use a hash map here, but this may not be the most efficient, as hash maps
+	tend to under perform when we're doing a bunch of insertions/deletions. I may consider using
+	a ordered map (red black tree) instead. Or, if the overhead ends up being too much, maybe just a regular
+	vector.
+	*/
+	std::unordered_map<EID, collisionHull*> content;
+	quadRegion *NE, *SE, *SW, *NW;
+	glm::vec2 NW_Corner, SE_Corner;
 };
 
 class quadTree
 {
-
+	quadRegion* root;
+	void update();
+	std::vector<collisionHull*> getNeigbors(quadRegion*);
+	void buildTree(std::vector<collisionHull>*);
+	void removeEntry(collisionHull*);
+	void addEntry(collisionHull*);
 };
