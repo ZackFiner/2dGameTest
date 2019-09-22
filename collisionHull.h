2b0515e66d42fb88c3b8f883ff38cbd9d5e22e42
ZackFiner <zackary.finer@yahoo.com>
2019-09-22 02:34:30 -0700

    Update collisionHull.h

--- collisionHull.h
+++ collisionHull.h
@@ -2,10 +2,16 @@
 #include "ofMain.h"
 struct boundingBox
 {
-	
+	std::vector<glm::vec2> verts;
+	float rot;
+	glm::vec2 pos;
 };
 
 class collisionHull
 {
-
+public:
+	glm::vec2 pos;
+	float rot;
+	boundingBox getBB();
+	boundingBox getAABB();
 };
