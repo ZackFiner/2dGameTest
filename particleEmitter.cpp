7a514b35c27e2cad198cf5c9af965f8761e7a7ac
Zack <zackary.finer@yahoo.com>
2019-10-26 02:54:18 -0700

    particle emitters
    when the player gets damaged enough, i want them to smoke. partices will be used for that. The physics requirment will also be met with this.

---
+++ particleEmitter.cpp
@@ -0,0 +1,43 @@
+#pragma once
+#include "particleEmitter.h"
+
+/*H*********************************************************
+ * FILE: particleEmitter.cpp
+ * AUTHOR: Zackary Finer
+ * DATE: 10/26/2019
+ *
+ * Description:
+ * This class contains the definition for a particle emitter
+ * interface, as well as a few concrete implementations
+ * of particle emitters.
+ *
+ * Particle Emitters are responsible for the systematic creation
+ * of particles.
+ *
+ ***********************************************************/
+
+void particleEmitter::setPos(const glm::vec2& pos)
+{
+	this->pos = pos;
+}
+glm::vec2 particleEmitter::getPos() const {
+	if (getParent() != nullptr)
+		return glm::rotate(pos, glm::radians(this->getRot())) + getParent()->getPos();
+	return pos;
+}
+void particleEmitter::setRot(float rot)
+{
+	this->rot = rot;
+}
+float particleEmitter::getRot() const
+{
+	if (getParent() != nullptr)
+		return rot + this->getRot();
+	return rot;
+}
+entity* particleEmitter::getParent() const {
+	return parent;
+}
+void particleEmitter::setParent(entity* parent) {
+	this->parent = parent;
+}
---
+++ particleEmitter.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "entity.h"
+#include "ofMain.h"
+/*H*********************************************************
+ * FILE: particleEmitter.h
+ * AUTHOR: Zackary Finer
+ * DATE: 10/26/2019
+ *
+ * Description:
+ * This class contains the definition for a particle emitter
+ * interface, as well as a few concrete implementations
+ * of particle emitters.
+ *
+ * Particle Emitters are responsible for the systematic creation
+ * of particles.
+ *
+ ***********************************************************/
+
+class particleEmitter
+{
+private:
+	entity* parent = nullptr;
+	glm::vec2 pos;
+	float rot;
+public:
+	virtual void update() = 0;
+	virtual void setPos(const glm::vec2&);
+	virtual glm::vec2 getPos() const;
+	virtual void setRot(float);
+	virtual float getRot() const;
+	virtual entity* getParent() const;
+	virtual void setParent(entity*);
+};
