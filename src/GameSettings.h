982916c31059c346b1af2469fd13207299631d0b
ZackFiner <zackary.finer@yahoo.com>
2019-10-27 22:49:16 -0700

    edits to spawning
    I've made it so tank spawning takes into considerations screen dimensions. Also working on adding a header file that will contain all game constants

---
+++ src/GameSettings.h
@@ -0,0 +1,6 @@
+#pragma once
+#define PLAY_AREA_SIZE glm::vec2(600,600)
+#define TANK_DEFAULT_HP 100
+#define TANK_LIFETIME 10.0f
+#define TANK_POINTS 20
+#define DESPAWN_RADIUS 100
--- src/tankSpawner.cpp
+++ src/tankSpawner.cpp
@@ -1,5 +1,6 @@
 #include "tankSpawner.h"
 #include "TankSprite.h"
+#include "GameSettings.h"
 /*H******************************************************************
  * FILENAME: tankSpawner.cpp
  * AUTHOR: Zackary Finer
@@ -20,21 +21,25 @@ tankSpawner::tankSpawner(entityManager* em, collisionManager* cm) :
 
 void tankSpawner::update()
 {
+	glm::vec2 border = glm::vec2(ofGetWidth(), ofGetHeight());
 	unsigned long cur = ofGetCurrentTime().getAsMilliseconds();
 	if (cur - last_spawn > (spawnRate*1000.0f))
 	{
+		int spawnWidth = border.x/2 + DESPAWN_RADIUS / 2;
+		int medianSpawnHeight = border.y / 4;
+		
 		bool on_left_side = ofRandomuf() > 0.5f;
 		bool linear = ofRandomuf() > 0.5f;
 		if (linear)
 			new tankSprite(manager, hitManager,
 				new linearPath(
-					glm::vec2(-600 * (on_left_side ? -1 : 1), 200),
-					glm::normalize(glm::vec2(600 * (on_left_side ? -1 : 1), ofRandom(-50.0f, 50.0f))), 
-					ofRandom(200.0f, 300.0f))
+					glm::vec2(-spawnWidth * (on_left_side ? -1 : 1), medianSpawnHeight),
+					glm::normalize(glm::vec2(spawnWidth * (on_left_side ? -1 : 1), ofRandom(-50.0f, 50.0f))),
+					ofRandom(medianSpawnHeight, medianSpawnHeight*1.5))
 				);
 		else
 			new tankSprite(manager, hitManager,
-				new sinPath(ofRandom(5.0f,50.0f), 100.0f, ofRandom(50.0f,150.0f), ofRandom(150.0f,250.0f), glm::vec2(-600*(on_left_side ? -1 : 1), 200), on_left_side));
+				new sinPath(ofRandom(5.0f,50.0f), 100.0f, ofRandom(medianSpawnHeight*0.25, medianSpawnHeight*0.75), ofRandom(medianSpawnHeight*0.75, medianSpawnHeight*1.25), glm::vec2(-spawnWidth *(on_left_side ? -1 : 1), medianSpawnHeight), on_left_side));
 
 
 		last_spawn = cur;
